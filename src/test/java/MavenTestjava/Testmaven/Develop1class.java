02c6e224e778cf2d30028d51bdf9eb294ea6623a
gvs2137 <singhgaurav2137@gmail.com>
2022-05-11 16:40:57 +0530

    For maven setup

--- src/test/java/MavenTestjava/Testmaven/Develop1class.java
+++ src/test/java/MavenTestjava/Testmaven/Develop1class.java
@@ -2,19 +2,24 @@ package MavenTestjava.Testmaven;
 
 public class Develop1class {
 
-	@Test
+	
 public void develop1method1()
 {
 	System.out.println("Maven selenium test 5");
 	System.out.println("Maven selenium test 5"); // Added new line
 }
-	@Test
+	
 public void develop1method2()
 {
 	System.out.println("Maven selenium test 5");
 	System.out.println("Maven selenium test 5"); // Added new line
 }
 	
-	
+	public static void main(String[] args) 
+	{
+		Develop1class d=new Develop1class();
+		d.develop1method1();
+		d.develop1method2();
+	}
 	
 }
