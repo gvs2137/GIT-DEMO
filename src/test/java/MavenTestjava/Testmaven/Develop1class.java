dea1cea6ec91d117febda95cf2f237c8e80d8295
gvs2137 <singhgaurav2137@gmail.com>
2022-05-11 14:11:18 +0530

    CG system

--- src/test/java/MavenTestjava/Testmaven/Develop1class.java
+++ src/test/java/MavenTestjava/Testmaven/Develop1class.java
@@ -14,4 +14,9 @@ public void develop1method2()
 	System.out.println("Maven selenium test 5");
 	System.out.println("Maven selenium test 5"); // Added new line
 }
+	public void develop1method3()
+	{
+		System.out.println("Maven selenium test 5");
+		System.out.println("Maven selenium test 5"); // Added new line
+	}
 }
